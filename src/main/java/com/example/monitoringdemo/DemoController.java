bd7dcb0c46889b5930ef80b92f941b8d7f77bce0
Marcin Kania <mkania86@gmail.com>
2017-10-27 12:46:08 +0200

    Playing with spring-boot 2

--- src/main/java/com/example/monitoringdemo/DemoController.java
+++ src/main/java/com/example/monitoringdemo/DemoController.java
@@ -1,33 +1,18 @@
 package com.example.monitoringdemo;
 
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Counter;
-import io.prometheus.client.exporter.common.TextFormat;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.io.Writer;
-
 @RestController
+@Order
 public class DemoController {
 
-    private final Counter promRequestsTotal = Counter.build()
-            .name("requests_total")
-            .help("Total number of requests.")
-            .register();
 
     @GetMapping
     public String demoEndpoint() {
-        promRequestsTotal.inc();
         return "Demo";
     }
 
-    @RequestMapping(path = "/prometheus")
-    public void metrics(Writer responseWriter) throws IOException {
-        TextFormat.write004(responseWriter, CollectorRegistry.defaultRegistry.metricFamilySamples());
-        responseWriter.close();
-    }
 
 }
