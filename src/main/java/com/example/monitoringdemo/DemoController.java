136d4d4403cb682bae8c8151444fae9471a8858c
Marcin Kania <mkania86@gmail.com>
2017-10-27 13:18:54 +0200

    Playing with spring-boot 2

--- src/main/java/com/example/monitoringdemo/DemoController.java
+++ src/main/java/com/example/monitoringdemo/DemoController.java
@@ -1,15 +1,13 @@
 package com.example.monitoringdemo;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Order
 public class DemoController {
 
 
-    @GetMapping
+    @GetMapping("/")
     public String demoEndpoint() {
         return "Demo";
     }
