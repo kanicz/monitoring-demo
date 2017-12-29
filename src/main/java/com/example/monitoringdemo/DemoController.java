d662fea7a92fd0ffcb040b77cc353cbd66460c9b
Marcin Kania <mkania86@gmail.com>
2017-12-29 11:14:27 +0100

    Added simple micrometer custom counter

--- src/main/java/com/example/monitoringdemo/DemoController.java
+++ src/main/java/com/example/monitoringdemo/DemoController.java
@@ -1,13 +1,18 @@
 package com.example.monitoringdemo;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    private final Counter demoRequests = Metrics.counter("Demo requests");
+
     @GetMapping
     public String demoEndpoint() {
+        demoRequests.increment();
         return "Demo";
     }
 
