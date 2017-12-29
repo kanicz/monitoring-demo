c611777305301399498c6ba525679a60449c04f5
Marcin Kania <mkania86@gmail.com>
2017-12-29 13:12:33 +0100

    Added simple conter with tags

--- src/main/java/com/example/monitoringdemo/DemoController.java
+++ src/main/java/com/example/monitoringdemo/DemoController.java
@@ -1,16 +1,20 @@
 package com.example.monitoringdemo;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    // Also interesting registry.config().commonTags(
+    private final Counter demoRequests = Metrics.counter("demo.http.requests","tag1", "value");
 
-    @GetMapping("/")
+    @GetMapping
     public String demoEndpoint() {
+        demoRequests.increment();
         return "Demo";
     }
-
 
 }
